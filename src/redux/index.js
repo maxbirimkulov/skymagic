0b31d10227af994a04cd769b4ce0c933135eca4b
maxbirimkulov <maxbirimkulov@mail.ru>
2022-09-27 15:17:46 +0600

    add boilerplate

---
+++ src/redux/index.js
@@ -0,0 +1,25 @@
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+
+const rootReducer = combineReducers({
+
+});
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck:false,
+        }),
+});
+
+export const persistor = persistStore(store)
+export default store
