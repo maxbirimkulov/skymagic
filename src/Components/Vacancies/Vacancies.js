3f3f048afb6a0c665c94ba9bba3141df448ae8bb
Nur Sadyraliev <tipbas6@gmail.com>
2022-10-14 17:45:57 +0600

    Vacancies

---
+++ src/Components/Vacancies/Vacancies.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Vacancies.scss'
+
+import {vacancies} from "./data";
+
+
+const Vacancies = () => {
+    return (
+        <div className='vacancies'>
+            <div className="vacancies__container container">
+                {
+                    vacancies && vacancies.map(el => (
+                        <div key={el.id} className="vacancies__content">
+                            <div className="vacancies__img">
+                                <img src={el.img} alt={`user${el.id}`}/>
+                            </div>
+                            <div className="vacancies__information">
+                                <h5 className="vacancies__category">{el.category}</h5>
+                                <div  className="vacancies__services">
+                                    <h3 className="vacancies__services-text">{el.services}</h3>
+                                </div>
+                                <p className="vacancies__price">{el.price}</p>
+                                <p className="vacancies__name">{el.name}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Vacancies;
