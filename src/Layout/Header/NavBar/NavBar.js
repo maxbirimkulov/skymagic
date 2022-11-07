081e2493a125ef27eeffb68a03546536de1ffb0b
Vindiza Aziz <vindizaaziz@gmail.com>
2022-11-07 17:42:15 +0600

    add dropdown links for parks

--- src/Layout/Header/NavBar/NavBar.js
+++ src/Layout/Header/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Navbar.scss"
 import {NavLink} from "react-router-dom";
+import Dropdown from "./Dropdown/Dropdown";
 
 
 const NavBar = ({toggleClass}) => {
     return (
         <ul className='nav container'>
-            <li className='nav__item'> <NavLink className="nav__link" to="/parks/park" onClick={toggleClass}>Наши Парки</NavLink></li>
+            <li className='nav__item'><Dropdown toggleClass={toggleClass}/></li>
             <li className='nav__item'> <NavLink className="nav__link" to="/reg" onClick={toggleClass}>Мероприятия</NavLink></li>
             <li className='nav__item'> <NavLink className="nav__link" to="/reg" onClick={toggleClass}>Новости</NavLink></li>
             <li className='nav__item'> <NavLink className="nav__link" to="/reg" onClick={toggleClass}>База знаний</NavLink></li>
--- src/pages/Park/Park.jsx
+++ src/pages/Park/Park.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Place from "./Place/Place";
 import Price from "./Price/Price";
-import {Navigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import Portfolio from "./Portfolio/Portfolio";
 import Map from "./Map/Map";
 
 import {data2} from "../../data.js"
 import AboutPark from "./AboutPark/AboutPark";
-let dataPark =  data2["Sky Park"]
 
 const Park = () => {
     const params = useParams()
-    if (params.name !== 'park') {
-        return <Navigate to='/'/>
-    }
+    // if (params.name !== 'park') {
+    //     return <Navigate to='/'/>
+    // }
+    let dataPark =  data2[`${params.name}`]
     return (
         <>
             <Place dataPark={dataPark}/>
