badb75ed2183d3f1c07eae792846de217ed7371d
Vindiza Aziz <vindizaaziz@gmail.com>
2022-11-23 18:32:59 +0600

    add price in park's card

--- src/pages/Park/Price/Price.js
+++ src/pages/Park/Price/Price.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import "./Price.scss"
 
-const Price = () => {
+const Price = ({dataPark}) => {
     return (
         <section className="hero-section">
             <div className="card-grid container">
-                <a className="card card2" href="#">
+                <div className="card card2">
                     <div className="card__background"></div>
                     <div className="card__content">
                         <p className="card__category">Category</p>
                         <h3 className="card__heading">Example Card Heading</h3>
+                        <p className="card__category">для детей от 1-года до 3х лет: </p>
+                        <h3 className="card__heading">{dataPark.price[0]}</h3>
                     </div>
-                </a>
-                <a className="card card3" href="#">
+                </div>
+                <div className="card card3">
                     <div className="card__background"></div>
                     <div className="card__content">
                         <p className="card__category">Category</p>
                         <h3 className="card__heading">Example Card Heading</h3>
+                        <p className="card__category">для детей от 4х лет и старше: </p>
+                        <h3 className="card__heading">{dataPark.price[1]}</h3>
                     </div>
-                </a>
+                </div>
 
             </div>
         </section>
