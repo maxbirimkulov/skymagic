b0f0f869d8a52acb611a18ad1e7f050822b34d90
Vindiza Aziz <vindizaaziz@gmail.com>
2022-11-28 17:15:18 +0600

    pipe update

--- src/pages/Park/Plan/Plan.js
+++ src/pages/Park/Plan/Plan.js
@@ -1,29 +1,52 @@
 import React, { useEffect, useRef } from "react";
+
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
 import "./Plan.scss";
+
 import { ReactComponent as HeroSvg } from "./heroN.svg";
 import ring from './ring.webp'
 import ringF from './ringF1.webp'
 import ringB from './ringB1.webp'
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-gsap.registerPlugin(MotionPathPlugin);
+
+
+
 export default function App() {
-    const textRef = useRef(null);
+    const textRef = useRef();
     const DeepRef = gsap.utils.selector(textRef);
     useEffect(() => {
-        let tl = gsap.timeline();
 
-            MotionPathPlugin.convertToPath("ellipse");
+        gsap.core.globals("ScrollTrigger", ScrollTrigger);
+        gsap.core.globals("MotionPathPlugin", MotionPathPlugin);
+        gsap.registerPlugin(ScrollTrigger,MotionPathPlugin);
 
-        tl.to(".slide-ring__ring", {
-            duration: 5,
-            repeat: -1,
+        MotionPathPlugin.convertToPath("ellipse");
+
+        let tl = gsap
+            .timeline(
+            {
+                defaults: {},
+                scrollTrigger: {
+                    trigger: textRef,
+                    start: "top top",
+                    end: "bottom top",
+                    scrub: 0.5,
+                    markers: true,
+                    toggleActions: "play pause resume reset"
+                }
+            }
+        )
+
+        .to(".slide-ring__ring", {
+            ease: "none",
+
             motionPath: {
                 path: DeepRef("#ellipse")[0],
                 align: DeepRef("#ellipse")[0],
                 alignOrigin: [1, 1]
             },
-            ease: "none"
         });
     }, []); // !!! Missing dependency array
 
