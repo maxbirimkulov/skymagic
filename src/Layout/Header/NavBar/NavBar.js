c48b52274123cea61dbc08f18e7c71ac71a7c7e7
Vindiza Aziz <vindizaaziz@gmail.com>
2022-12-19 17:55:45 +0600

    upgrade header

--- src/Layout/Header/NavBar/NavBar.js
+++ src/Layout/Header/NavBar/NavBar.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import "./Navbar.scss"
 import {NavLink} from "react-router-dom";
-import Dropdown from "./Dropdown/Dropdown";
 
 
 const NavBar = ({toggleClass}) => {
     return (
         <ul className='nav container'>
-            <li className='nav__item'><Dropdown toggleClass={toggleClass}/></li>
-            <li className='nav__item'> <NavLink className="nav__link" to="/reg" onClick={toggleClass}>Мероприятия</NavLink></li>
-            <li className='nav__item'> <NavLink className="nav__link" to="/reg" onClick={toggleClass}>Новости</NavLink></li>
-            <li className='nav__item'> <NavLink className="nav__link" to="/account/info" onClick={toggleClass}>Мой аккаунт</NavLink></li>
-            <li className='nav__item'> <NavLink className="nav__link" to="/reg" onClick={toggleClass}>Контакты</NavLink></li>
-            <li className='nav__item'> <NavLink className="nav__link" to="/career" onClick={toggleClass}>Карьера</NavLink></li>
+
+
+            <li className='nav__item'><NavLink className="nav__link" to="/parks/Sky Park" onClick={toggleClass}>Sky
+                park</NavLink></li>
+            <li className='nav__item'><NavLink className="nav__link" to="/parks/Smagic Techno" onClick={toggleClass}>Smagic
+                Технопарк</NavLink></li>
+            <li className='nav__item'><NavLink className="nav__link" to="/parks/Smagic Tsum" onClick={toggleClass}>Smagic
+                Цум 2</NavLink></li>
+            <li className='nav__item'><NavLink className="nav__link" to="/parks/Smagic Tommi" onClick={toggleClass}>Smagic
+                Tommi mall</NavLink></li>
+            <li className='nav__item'><NavLink className="nav__link" to="/account/info" onClick={toggleClass}>Мой
+                аккаунт</NavLink></li>
+            <li className='nav__item'><NavLink className="nav__link" to="/reg" onClick={toggleClass}>Контакты</NavLink>
+            </li>
+            <li className='nav__item'><NavLink className="nav__link" to="/career"
+                                               onClick={toggleClass}>Карьера</NavLink></li>
         </ul>
     );
 };
