2d16e63893ea3ac3a730d767b1aefbf26ab0e0ab
Vindiza Aziz <vindizaaziz@gmail.com>
2023-04-08 19:15:40 +0600

    fix header and some media

--- src/pages/Home/Baner/Baner.jsx
+++ src/pages/Home/Baner/Baner.jsx
@@ -56,6 +56,41 @@ const Baner = () => {
                         ))}
                     </Swiper>
                 </div>
+                <div className="banner__second-slider">
+                    <Swiper
+                        // autoplay={{
+                        //     delay: 2500,
+                        //     disableOnInteraction: false,
+                        // }}
+                        slidesPerView={"auto"}
+                        centeredSlides={true}
+                        spaceBetween={30}
+
+                        modules={[Autoplay]}
+                        className="banner__second-swiper"
+                    >
+                        <div className="swiper-slide-wrapper">
+                            {data.map((el)=>(
+                                <SwiperSlide key={el} className={"banner__title"}>
+                                    <h2 className='banner__smagic'>
+                                        {el.title}
+                                    </h2>
+                                    <h2 className="banner__smagic">
+                                        {el.title2}
+                                    </h2>
+
+                                    <h4 className='banner__text'>
+                                        {el.description}
+                                    </h4>
+
+                                    <Link  to='/ticket'><button className='banner__btn'>Купить билет</button></Link>
+                                </SwiperSlide>
+                            ))}
+
+                        </div>
+
+                    </Swiper>
+                </div>
             </div>
         </section>
     );
