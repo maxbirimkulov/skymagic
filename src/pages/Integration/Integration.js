854f609102a433624129ecaaba7efd44800e7292
alibek <alibek1024@mail.ru>
2023-04-09 21:26:41 +0600

    add partners

--- src/pages/Integration/Integration.js
+++ src/pages/Integration/Integration.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Integration.scss'
 import img1 from '../franchise/Asset 17.png'
 import ocak from './Ожак Кебаб.png'
@@ -9,8 +9,33 @@ import bakai from './Бакай Банк.png'
 import bublik from './Бублик.png'
 import dodo from './Додо пицца.png'
 import dolce from './Дольче Вита.png'
+import {IoMdClose} from 'react-icons/io'
 
 const Integration = () => {
+    const [popup,setPopup]=useState('')
+
+    const partner = [
+        {name:'bakai',
+        img:'https://thumb.tildacdn.com/tild3737-6130-4036-b135-656263643463/-/resize/760x/-/format/webp/0293_1.jpg',
+            text:'qwerrtyui'
+        },
+        {name:'dodo',
+            img:'',
+            text:'qwerrtyui'
+        },
+        {name:'dolce',
+            img:'',
+            text:'qwerrtyui'
+        },
+        {name:'bublik',
+            img:'',
+            text:'qwerrtyui'
+        },
+        {name:'ocak',
+            img:'',
+            text:'qwerrtyui'
+        }
+    ]
     return (
         <section className='integration'>
             <div className="container">
@@ -41,14 +66,37 @@ const Integration = () => {
                 </div>
                 <h2 className='integration__partners'>НАШИ ПАРТНЕРЫ:</h2>
                 <div className='integration__partner'>
-                    <img src={ocak} alt=""/>
-                    <img src={bakai} alt=""/>
-                    <img src={dolce} alt=""/>
-                    <img src={bublik} alt=""/>
-                    <img src={dodo} alt=""/>
+                    <img onClick={(e)=>{setPopup(e.target.id)}} id='ocak' src={ocak} alt=""/>
+                    <img onClick={(e)=>{setPopup(e.target.id)}} id='bakai' src={bakai} alt=""/>
+                    <img onClick={(e)=>{setPopup(e.target.id)}} id='dolce' src={dolce} alt=""/>
+                    <img onClick={(e)=>{setPopup(e.target.id)}} id='bublik' src={bublik} alt=""/>
+                    <img onClick={(e)=>{setPopup(e.target.id)}} id='dodo' src={dodo} alt=""/>
 
                 </div>
             </div>
+
+            <div className='integration__popup'>
+                {
+                    partner.filter((el)=>(
+                            el.name === popup
+                        )
+                    ).map((el,idx)=>(
+                        <div className='integration__popup-show' key={el.name}>
+                            <div className="popup-back">
+                                <div className='integration__img'>
+                                    <img src={el.img} alt={el.name}/>
+                                </div>
+                                <div className="integration__p">
+                                    {el.text}
+                                </div>
+                            </div>
+                            <h2 className='integration__close' onClick={()=>setPopup('')}><IoMdClose/></h2>
+                        </div>
+
+                    ))
+                }
+            </div>
+
         </section>
     );
 };
