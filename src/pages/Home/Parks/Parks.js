f1f698477618a1916dd52fa6646335930333fd83
alibek <alibek1024@mail.ru>
2023-05-22 19:41:00 +0600

    fix imgs

--- src/pages/Home/Parks/Parks.js
+++ src/pages/Home/Parks/Parks.js
@@ -21,45 +21,41 @@ const Parks = () => {
                 <h2 className='parks__branches '>НАШИ ПАРКИ</h2>
                 <div className="parks__cards">
                     <div className="parks__card">
-
-                        <img src={tehnopark} alt=""/>
-
-
+                        <NavLink onClick={()=>toTop()} to='/parks/Smagic%20Techno'>
+                            <img  src={tehnopark} alt=""/>
+                        </NavLink>
                         <div className="parks__text">
                             <h3>SMAGIC <span>ТЕХНОПАРК</span></h3>
                         </div>
                         <NavLink onClick={()=>toTop()} to='/parks/Smagic%20Techno'><button>ПОДРОБНЕЕ</button></NavLink>
-
                     </div>
-
                     <div className="parks__card">
-
-                        <img src={stum} alt=""/>
-
+                        <NavLink onClick={()=>toTop()} to='/parks/Smagic%20Tsum'>
+                            <img  src={stum} alt=""/>
+                        </NavLink>
                         <div className="parks__text">
                             <h3>SMAGIC <span>ЦУМ</span></h3>
                         </div>
                         <NavLink onClick={()=>toTop()} to='/parks/Smagic%20Tsum'><button>ПОДРОБНЕЕ</button></NavLink>
                     </div>
                     <div className="parks__card">
-
-                            <img src={tommi} alt=""/>
-
+                        <NavLink onClick={()=>toTop()} to='/parks/Smagic%20Tommi'>
+                            <img  src={tommi} alt=""/>
+                        </NavLink>
                         <div className="parks__text">
                             <h3>SMAGIC <span>TOMMI MALL</span></h3>
                         </div>
                         <NavLink onClick={()=>toTop()} to='/parks/Smagic%20Tommi'><button>ПОДРОБНЕЕ</button></NavLink>
                     </div>
                     <div className="parks__card">
-
-                        <img src={skypark} alt="sky-park"/>
-
+                        <NavLink onClick={()=>toTop()} to='/parks/Sky%20Park'>
+                            <img  src={skypark} alt="sky-park"/>
+                        </NavLink>
                         <div className="parks__text">
                             <h3>SKY PARK <span>АЛА-АРЧА</span></h3>
                         </div>
                         <NavLink onClick={()=>toTop()} to='/parks/Sky%20Park'><button>ПОДРОБНЕЕ</button></NavLink>
                     </div>
-
                     </div>
                 </div>
         </section>
